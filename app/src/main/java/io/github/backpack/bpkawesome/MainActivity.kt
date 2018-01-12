09d18ba0fe37f2336318f91757435e98ab6d9232
Matteo Hertel <info@matteohertel.com>
2018-01-12 15:34:48 +0000

    Add working android app

---
+++ app/src/main/java/io/github/backpack/bpkawesome/MainActivity.kt
@@ -0,0 +1,31 @@
+package io.github.backpack.bpkawesome
+
+import kotlinx.android.synthetic.main.activity_main.textView
+import android.media.TimedText
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sohowToast(view: View){
+        Toast.makeText(this, "Hello, World!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    fun incrementCount (view: View){
+        val countString = textView.text.toString()
+
+        var count: Int = Integer.parseInt(countString)
+        count++
+        textView.text = count.toString();
+    }
+}
